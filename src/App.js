847de985e20bedd37c2f348df18a0d9b0206c5e2
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-02-14 20:19:19 +0100

    added everything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Clock from "./Clock";
+import Clock from "./Clock.js";
 
 function App() {
   return (
--- src/Clock.js
+++ src/Clock.js
@@ -1,13 +1,61 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Clock.css";
 
 function Clock() {
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+
+  useEffect(() => {
+    let dt = new Date();
+    dt = dt.toLocaleTimeString(navigator.language, {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    let h = dt.split(":")[0];
+    const m = dt.split(":")[1];
+    const s = dt.split(":")[2];
+    if (h > 12) {
+      h = h - 12;
+    }
+    setHour((hour) => hour + h * 30 + (m * 1) / 2 + (s * 1) / 120);
+    setMin((min) => min + m * 6 + (s * 1) / 10);
+    setSec((sec) => sec + s * 6);
+
+    setInterval(() => {
+      setHour((hour) => hour + 1 / 120);
+      setMin((min) => min + 1 / 10);
+      setSec((sec) => sec + 6);
+    }, 1000);
+  }, []);
+
   return (
     <div className="clock">
       <div className="clock-face">
-        <div className="hand hour-hand"></div>
-        <div className="hand min-hand"></div>
-        <div className="hand second-hand"></div>
+        <div
+          className="hand hour-hand"
+          style={{
+            transform: `rotate(${hour}deg)`,
+            transformOrigin: "center bottom",
+          }}
+        ></div>
+        <div
+          className="hand min-hand"
+          style={{
+            transform: `rotate(${min}deg)`,
+            transformOrigin: "center bottom",
+          }}
+        ></div>
+        <div
+          className="hand sec-hand"
+          style={{
+            transform: `rotate(${sec}deg)`,
+            transformOrigin: "center bottom",
+          }}
+        ></div>
+        <div className="circle"></div>
       </div>
     </div>
   );
